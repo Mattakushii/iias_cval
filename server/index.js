1b54ce7c513603ba1e0ebf90ef5a0e1b75f17f4e
Шунский <pas4o@yandex.ru>
2021-05-23 13:38:44 +0400

    Fix set headers error

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const app = express();
+
+const useSocket = require("socket.io")
+const server = require('http').Server(app)
+const io = useSocket(server)
+
 const db = require("./settings/db");
+const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 3001;
 
-const verifyJWT = (req, res, next) => {
+const verifyJWT = (req, res) => {
   const token = req.headers["x-access-token"];
 
   if (!token) {
     console.log("Token not found");
-    res.send("Token not found");
   } else {
     jwt.verify(token, "zxc", (err) => {
       if (err) {
-        console.log("Token not found");
-        res.send({ isAuth: false, mes: "Token error" });
+        console.log("Token not valid");
+        return res.send({ isAuth: false, message: "Token error" });
       } else {
-        res.send({ isAuth: true, mes: "Token proceed" });
-        next();
+        return res.send({ isAuth: true, message: "Token proceed" });
       }
     });
   }
@@ -33,7 +36,11 @@ app.get("/api/students", (req, res) => {
   db.query(
     "SELECT first_name, second_name, is_auth FROM students",
     (err, result) => {
-      res.json(result);
+      if (!err) {
+        res.json(result);
+      } else {
+        res.json({ dbError: true, message: "Database error" });
+      }
     }
   );
 });
@@ -42,25 +49,32 @@ app.get("/api/isAuth", verifyJWT, (req, res) => {
   res.send("Auth success");
 });
 
+io.on('connection', socket => {
+  console.log("socket ready", socket)
+})
+
 app.post("/api/login", (req, res) => {
   const login = req.body.login;
   const password = req.body.password;
 
   db.query(findUser(login, password), (err, result) => {
     if (err) {
-      res.send({ err: err });
-    }
+      console.log("1")
+      return res.send({ err: err });
+    } else 
 
     if (result.length > 0) {
+      console.log("2")
       const token = jwt.sign({ id: result.id }, "zxc", { expiresIn: 240 });
-      res.json({
+      return res.json({
         isAuth: true,
         first_name: result[0].first_name,
         second_name: result[0].second_name,
         token: token,
       });
     } else {
-      res.send("Unknown user");
+      console.log("3")
+      return res.json({ isAuth: false, message: "invalid user or password" });
     }
   });
 });

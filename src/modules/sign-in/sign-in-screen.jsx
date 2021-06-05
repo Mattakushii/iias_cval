fa7ca461ea2923e65503537ea0fd14d14f30f1fb
Kostya <romanovkostya1999@gmail.com>
2021-06-05 09:53:29 +0400

    add sing-in-screen module & add imgs

--- src/modules/sign-in/sign-in-screen.jsx
+++ src/modules/sign-in/sign-in-screen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-
+import './style.css';
+import OpenModal from './script.js';
 import { useAuthContext } from "../../core/auth-provider"
 
 export const SignInScreen = () => {
@@ -10,22 +11,39 @@ export const SignInScreen = () => {
 
     return (
         <>
-            <label className="login-form__label">Логин</label>
-            <input
-                className="login-form__input"
-                value={login}
-                onChange={(e) => setLogin(e.target.value)}
-            ></input>
-            <label className="login-form__label">Пароль</label>
-            <input
-                className="login-form__input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-            ></input>
-            <button onClick={() => authContext.login(login, password)} type="button">
-                Войти
-            </button>
+            <header>
+                <div className="main-bg">
+                    <img className="main-logo" src='img/logo.png' alt="Logo" />
+                    <button onClick={OpenModal} className="btn main-registration">Регистрация</button>
+                    <button onClick={OpenModal} className="btn main-login">Войти</button>
+                    <div className="main-title">
+                        <h1 className="main-title1">Центр</h1>
+                        <h1 className="main-title2">Информационного</h1>
+                        <h1 className="main-title3">Обеспечения</h1>
+                    </div>
+                </div>
+                <div className="modal">
+                    <div className="modal-bg"></div>
+                    <div className="modal-content">
+                        <h3 className="modal-title">Здравствуйте!<br />Чтобы войти в систему введите логин и пароль</h3>
+                        <form className="modal-form" >
+                            <input className="modal-input" type="text" placeholder="Логин" 
+                                required 
+                                value={login} 
+                                onChange={(e) => setLogin(e.target.value)}
+                            ></input>
+                            <input className="modal-input" type="password" placeholder="Пароль" 
+                                required 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            ></input>
+                            <button className="modal-btn" onClick={() => authContext.login(login, password)} type="button">
+                                Войти
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </header>
         </>
     )
 }
